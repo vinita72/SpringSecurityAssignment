cbb25bab0c51b49a2d11a9fa15caf802305785be
vinita <imvinitarajoria@gmail.com>
2021-05-15 02:23:07 +0530

    Security

---
+++ SpringSecurity/src/main/java/com/securitySpring/SpringSecurity/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.securitySpring.SpringSecurity;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+	
+@RequestMapping("/")
+	public String home() {
+			return "home.jsp";
+		}
+			
+			
+		}
+	
+	
+
+
