a0cef7b362ce31b29251f7629e59d7fef469f59d
vinita <imvinitarajoria@gmail.com>
2021-05-15 21:54:28 +0530

    JWT Spring security

---
+++ SpringSecurityLogInEncryptio/src/test/java/com/SpringSecurityLogInEncryptio/SpringSecurityLogInEncryptio/SpringSecurityLogInEncryptioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringSecurityLogInEncryptio.SpringSecurityLogInEncryptio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLogInEncryptioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
